08cc23d9a7abc8f01c43e655c9a5df135e1c33eb
willymariino <wg.mariino@gmail.com>
2025-03-07 17:34:02 +0100

    created object array

---
+++ snack 2/script.js
@@ -0,0 +1,17 @@
+let squadre = [
+    {
+        nome: "lions",
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+    {
+        nome: "bears",
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+    {
+        nome: "wolves",
+        puntiFatti: 0,
+        falliSubiti: 0
+    }
+];
