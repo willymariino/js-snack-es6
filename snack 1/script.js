c987450bf2653d6f6d75a623f42abb9eec473691
willymariino <wg.mariino@gmail.com>
2025-03-07 17:12:07 +0100

    finished snack 1

--- snack 1/script.js
+++ snack 1/script.js
@@ -23,4 +23,8 @@ for (let [i] in bikes) {
     }
 
 }
-console.log("la bici più pesante è:")
+console.log("la bici più pesante è:", heaviestBike.marca, "con un peso di kg", heaviestBike.peso);
+
+const bikeWeight = document.getElementById("bike-weight")
+
+bikeWeight.textContent = "la bici più pesante è" + " " + heaviestBike.marca + " " + "con un peso di kg" + "" + heaviestBike.peso
