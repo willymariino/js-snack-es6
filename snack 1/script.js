30c61420a00617de7b80526e05e1c56d4fac0c3e
willymariino <wg.mariino@gmail.com>
2025-03-07 15:17:57 +0100

    array object snack 1

---
+++ snack 1/script.js
@@ -0,0 +1,16 @@
+let biciclette = [
+    {
+        marca: "Atala",
+        peso: 30
+    },
+    {
+        marca: "Brera",
+        peso: 25
+    },
+    {
+        marca: "Agazzini",
+        peso: 28
+    }
+];
+
+
